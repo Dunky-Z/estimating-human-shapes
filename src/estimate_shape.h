63e66f87bb53221555f2acecb95261a72e25267d
如云泊 <254758318@qq.com>
2020-07-04 10:57:06 +0800

    init

--- src/estimate_shape.h
+++ src/estimate_shape.h
@@ -13,24 +13,16 @@ public:
 	~Estimate();
 	void Apply();
 	float CheckIntersection(pmp::vec3 intersect_point, const pmp::Edge& edg, const pmp::vec3& normal, const pmp::vec3& v, const SurfaceMesh & mesh);
-	void CalcIntersectionPoint(pmp::vec3 intersect_point, const SurfaceMesh& mesh, const float scale, const pmp::vec3& p0, const pmp::vec3& p1);
+	void CalcIntersectionPoint(pmp::vec3& intersect_point, const float& scale, const SurfaceMesh& mesh, const pmp::vec3& p0, const pmp::vec3& p1);
 
 	void SetBool(SurfaceMesh&mesh);
-	float CalcChainLength(std::vector<pmp::vec3> intersect_points);
-	void CalcIntersectionPoints(std::vector<pmp::vec3> intersect_points, std::vector<pmp::Edge> edgs_intersect, std::vector<float> scale_set,
+	float CalcChainLength(std::vector<pmp::vec3>& intersect_points);
+	void CalcIntersectionPoints(std::vector<pmp::vec3>& intersect_points, std::vector<pmp::Edge>& edgs_intersect, std::vector<float>& scale_set,
 		SurfaceMesh& mesh, const pmp::vec3& normal, const pmp::vec3& v);
 
-	void FindIntersectionEdgeNearby(pmp::EdgeProperty<bool> is_checked, std::vector<float> scale_set, std::vector<pmp::vec3> intersect_points,
-		std::vector<pmp::Edge> edgs_intersect, const SurfaceMesh& mesh, const pmp::Edge& edg, const pmp::vec3& normal, const pmp::vec3& v, pmp::vec3 intersect_point);
+	void FindIntersectionEdgeNearby(pmp::EdgeProperty<bool>& is_checked, std::vector<float>& scale_set, std::vector<pmp::vec3>& intersect_points,
+		std::vector<pmp::Edge>& edgs_intersect, const SurfaceMesh& mesh, const pmp::Edge& edg, const pmp::vec3& normal, const pmp::vec3& v, pmp::vec3 intersect_point);
 private:
 	const pmp::vec3 normal = vec3(0, 0, 1);
+	const pmp::vec3 ori_point = vec3(0, 0, 0);
 };
-
-Estimate::Estimate()
-{
-}
-
-Estimate::~Estimate()
-{
-}
-
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "estimate_shape.h"
+
+
+int main()
+{
+	Estimate estimate;
+	estimate.Apply();
+
+	getchar();
+	return 0;
+}
